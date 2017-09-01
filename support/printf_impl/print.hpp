512b3098ee5c80579db041969e9f3ee5c139fca4
Frank Richter <frank.richter@gmail.com>
2017-09-01 14:03:41 +0200

    Slightly reorganize printf implementation

--- support/printf_impl/print.hpp
+++ support/printf_impl/print.hpp
@@ -35,41 +35,72 @@
 
 namespace printf_impl
 {
+    struct PadInfo
+    {
+        bool need_pad;
+        bool left_pad;
+        bool right_pad;
+        char pad_char;
+        int pad_num;
+
+        PadInfo (const FormatFWP& fwp, int out_width)
+            : need_pad (out_width < fwp.width),
+              left_pad (need_pad & ((fwp.flags & ffLeftAlign) == 0)),
+              right_pad (need_pad & ((fwp.flags & ffLeftAlign) != 0)),
+              pad_char (((fwp.flags & (ffZeroPad | ffLeftAlign)) == ffZeroPad) ? '0' : ' '),
+              pad_num (fwp.width - out_width)
+        {}
+    };
+
+    static int print_preamble (Sink& sink, const PadInfo& pad)
+    {
+        int return_val = 0;
+        if (pad.left_pad)
+        {
+            for (int i = 0; i < pad.pad_num; i++)
+            {
+                int pad_ret = sink.ascii (&pad.pad_char, 1);
+                if (pad_ret < 0) return pad_ret;
+                return_val += pad_ret;
+            }
+        }
+        return return_val;
+    }
+
+    static int print_postamble (Sink& sink, const PadInfo& pad)
+    {
+        int return_val = 0;
+        if (pad.right_pad)
+        {
+            for (int i = 0; i < pad.pad_num; i++)
+            {
+                int pad_ret = sink.ascii (&pad.pad_char, 1);
+                if (pad_ret < 0) return pad_ret;
+                return_val += pad_ret;
+            }
+        }
+        return return_val;
+    }
+
     template<typename Printer, typename... Arg>
     static int print (Sink& sink, const FormatFWP& fwp, va_list& args, Arg... printer_arg)
     {
         Printer printer (fwp, args, std::forward<Arg> (printer_arg)...);
 
-        int return_val = 0;
         int out_width = printer.out_len();
-        bool need_pad = (out_width < fwp.width);
-        bool left_pad = need_pad & ((fwp.flags & ffLeftAlign) == 0);
-        bool right_pad = need_pad & ((fwp.flags & ffLeftAlign) != 0);
-        char pad_char = ((fwp.flags & (ffZeroPad | ffLeftAlign)) == ffZeroPad) ? '0' : ' ';
-        int pad_num = fwp.width - out_width;
-        if (left_pad)
-        {
-            for (int i = 0; i < pad_num; i++)
-            {
-                int pad_ret = sink.ascii (&pad_char, 1);
-                if (pad_ret < 0) return pad_ret;
-                return_val += pad_ret;
-            }
-        }
+        PadInfo pad (fwp, out_width);
+
+        int return_val = print_preamble (sink, pad);
+        if (return_val < 0) return return_val;
 
         int print_ret = printer.print (sink);
         if (print_ret < 0) return print_ret;
         return_val += print_ret;
 
-        if (right_pad)
-        {
-            for (int i = 0; i < pad_num; i++)
-            {
-                int pad_ret = sink.ascii (&pad_char, 1);
-                if (pad_ret < 0) return pad_ret;
-                return_val += pad_ret;
-            }
-        }
+        int postamble_ret = print_postamble (sink, pad);
+        if (postamble_ret < 0) return postamble_ret;
+        return_val += postamble_ret;
+
         return return_val;
     }
 
