c53e54cca301266c2fe71ed2e2a601559ee78ef6
Frank Richter <frank.richter@gmail.com>
2017-11-05 19:07:15 +0100

    Also implement memset() using NT runtime

--- support/memory.cpp
+++ support/memory.cpp
@@ -1,9 +1,11 @@
 #include <windows.h>
 
+#undef RtlFillMemory
 #undef RtlMoveMemory
 
 #pragma function (memcmp)
 #pragma function (memcpy)
+#pragma function (memset)
 
 /* Default memory functions implemented using NT runtime functions.
  * Using the NT runtime functions reduces the implementation footprint. */
@@ -29,4 +31,12 @@ extern "C" void* memmove (void* dest, const void* src, size_t num)
 {
   RtlMoveMemory (dest, src, num);
   return dest;
+}
+
+extern "C" NTSYSAPI VOID NTAPI RtlFillMemory (VOID*, SIZE_T, UCHAR);
+
+extern "C" void* memset (void* dst, int value, size_t num)
+{
+  RtlFillMemory (dst, num, static_cast<UCHAR> (value));
+  return dst;
 }
