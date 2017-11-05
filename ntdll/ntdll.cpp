8d581b4409bf222ab6e29980f9af62dc67a5fc31
Frank Richter <frank.richter@gmail.com>
2017-11-05 19:00:13 +0100

    Also implement memcpy(), memmove() using NT runtime

--- ntdll/ntdll.cpp
+++ ntdll/ntdll.cpp
@@ -1,3 +1,4 @@
 #include <basetsd.h>
 
 extern "C" SIZE_T __stdcall RtlCompareMemory (const void*, const void*, SIZE_T) { return 0; }
+extern "C" void __stdcall RtlMoveMemory (void*, const void*, SIZE_T) {}
--- support/memory.cpp
+++ support/memory.cpp
@@ -1,6 +1,9 @@
 #include <windows.h>
 
+#undef RtlMoveMemory
+
 #pragma function (memcmp)
+#pragma function (memcpy)
 
 /* Default memory functions implemented using NT runtime functions.
  * Using the NT runtime functions reduces the implementation footprint. */
@@ -12,4 +15,18 @@ extern "C" int memcmp (const void* ptr1, const void* ptr2, size_t num)
   unsigned char c1 = reinterpret_cast<const unsigned char*> (ptr1)[equal_size];
   unsigned char c2 = reinterpret_cast<const unsigned char*> (ptr2)[equal_size];
   return c1 < c2 ? -1 : 1;
+}
+
+extern "C" NTSYSAPI VOID NTAPI RtlMoveMemory (VOID*, const VOID*, SIZE_T);
+
+extern "C" void* memcpy (void* dest, const void* src, size_t num)
+{
+  RtlMoveMemory (dest, src, num);
+  return dest;
+}
+
+extern "C" void* memmove (void* dest, const void* src, size_t num)
+{
+  RtlMoveMemory (dest, src, num);
+  return dest;
 }
