4f77fd243c95262fbfb9b91d888bff66369588cf
Frank Richter <frank.richter@gmail.com>
2019-04-13 16:50:22 +0200

    Add additional string length functions

--- support/strnlen.cpp
+++ support/strnlen.cpp
@@ -1,3 +1,27 @@
+extern "C" size_t strnlen (const char* str, size_t numberOfElements)
+{
+  size_t n = 0;
+  const char* p = str;
+  while ((*p != 0) && (n < numberOfElements))
+  {
+    p++;
+    n++;
+  }
+  return n;
+}
+
+extern "C" size_t wcslen (const wchar_t* str)
+{
+  size_t n = 0;
+  const wchar_t* p = str;
+  while (*p != 0)
+  {
+    p++;
+    n++;
+  }
+  return n;
+}
+
 extern "C" size_t wcsnlen (const wchar_t* str, size_t numberOfElements)
 {
   size_t n = 0;
